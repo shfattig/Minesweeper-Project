c7c9130fdbeb09c1944005a1e86aa229321bf8b4
shfattig <sfattig@tamu.edu>
2015-12-02 00:48:44 -0600

    Update Square.cpp

--- minesweeperV2.0/Square.cpp
+++ minesweeperV2.0/Square.cpp
@@ -24,3 +24,40 @@ State Square::getState() {
 bool Square::checkIfCovered() {
 	return false;
 }
+bool Square::checkFlag()
+{
+	return Flag;
+}
+
+void Square::checkPics(Fl_JPEG_Image* hellboy, Fl_JPEG_Image* one, Fl_JPEG_Image* two, Fl_JPEG_Image* three, Fl_JPEG_Image* four, Fl_JPEG_Image* five,
+	Fl_JPEG_Image* six, Fl_JPEG_Image* seven, Fl_JPEG_Image* eight) {
+		{
+			if (one->w() == 0 || one->h() == 0 || one->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"one.jpg\"" << endl;
+			}
+			if (two->w() == 0 || two->h() == 0 || two->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"two.jpg\"" << endl;
+			}
+			if (three->w() == 0 || three->h() == 0 || three->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"three.jpg\"" << endl;
+			}
+			if (four->w() == 0 || four->h() == 0 || four->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"four.jpg\"" << endl;
+			}
+			if (five->w() == 0 || five->h() == 0 || five->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"five.jpg\"" << endl;
+			}
+			if (six->w() == 0 || six->h() == 0 || six->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"six.jpg\"" << endl;
+			}
+			if (seven->w() == 0 || seven->h() == 0 || seven->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"seven.jpg\"" << endl;
+			}
+			if (eight->w() == 0 || eight->h() == 0 || eight->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"eight.jpg\"" << endl;
+			}
+			if (hellboy->w() == 0 || hellboy->h() == 0 || hellboy->d() == 0) { //check to see if image opened.
+				cerr << "ERROR loading \"hell_boy.jpg\"" << endl;
+			}
+		}
+}
